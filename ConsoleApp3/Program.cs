c3d8555d092977311641b6bfcf1dfde0c041f671
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2023-01-24 18:38:31 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.1
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    Console.Write("#");
+            //}
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    Console.WriteLine("#");
+            //}
+            //Console.ReadKey();
+
+            //1.2
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Console.WriteLine();
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    Console.WriteLine("         #");
+            //}
+            //Console.ReadKey();
+
+            //1.3
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    Console.WriteLine("#");
+            //}
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Console.ReadKey();
+
+            //1.4
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    Console.WriteLine("         #");
+            //}
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Console.ReadKey();
+
+            //2
+            //for (int i = 1; i < 17; i++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Console.WriteLine();
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    Console.WriteLine("#              #");
+            //}
+            //for (int i = 1; i < 17; i++)
+            //{
+            //    Console.Write("#");
+            //}
+            //Console.ReadKey();
+
+            //3
+            //for (int i = 1; i < 6; i++)
+            //{
+            //    for (int k = 0; k < 6 - i; k++)
+            //        Console.Write($"{k} "); ;
+            //    Console.WriteLine();
+            //}
+            //Console.ReadKey();
+
+            //4
+            for (int i = 1; i < 5; i++)
+            {
+                for (int y = 1; y < 6; y++)
+                {
+                    Console.Write("2   ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();6
+
+
+
+
+        }
+}   }
